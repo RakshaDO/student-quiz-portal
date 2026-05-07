9922e791289a0c791dd7b8638e6c8aa3269b1fd4
Adiveppa Mamadapur <adiveppadm7@gmail.com>
2026-05-07 09:26:42 +0530

    Updated Flask host configuration

--- app.py
+++ app.py
@@ -480,4 +480,5 @@ def leaderboard():
     return render_template('leaderboard.html', leaders=leaders)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
